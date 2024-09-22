b81dff66f2c3889ffc2ad8916b942c5743f9ef45
nirobahmed0298 <nirobahmed0298@gmail.com>
2024-09-22 23:40:19 +0600

    home page added

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function showBtn(id){
+    // Hidden Btn
+    document.getElementById('donation').classList.add('hidden');
+    document.getElementById('history').classList.add('hidden');
+    // Show Btn Id
+    document.getElementById(id).classList.remove('hidden');
+
+}
+document.getElementById('donation-btn').addEventListener('click',function(){
+    document.getElementById('donation-btn').classList.add('bg-btn_color','border-none');
+    document.getElementById('history-btn').classList.remove('bg-btn_color','border-none')
+    // Show Btn
+    showBtn('donation');
+})
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('history-btn').classList.add('bg-btn_color','border-none');
+    document.getElementById('donation-btn').classList.remove('bg-btn_color','border-none')
+
+    // Show Btn
+    showBtn('history');
+})
